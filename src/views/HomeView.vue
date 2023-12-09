ee33fb187456890e33f6f1ec8f462d207a040f47
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-09 19:20:46 +0100

    feat: added test filterRow

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,19 @@
 <script setup lang="ts">
 	import { onMounted } from "vue"
 	import { useProducts } from "@/stores/products"
-	import type { CategoryName } from "@/types/Product"
 
-	import { storeToRefs } from "pinia"
+	import FilterRow from "@/components/FilterRow.vue"
 
-	const { getProducts, getCategories, setSelectedCategory } = useProducts()
-	const { products, categories, selectedCategory } = storeToRefs(useProducts())
+	const { getProducts, getCategories } = useProducts()
 
 	onMounted(async () => {
 		await getCategories()
 		await getProducts()
 	})
-
-	const handleChange = (event) => {
-		setSelectedCategory(event.target.value as CategoryName)
-		getProducts()
-	}
 </script>
 
 <template>
 	<main>
-		<select @change="handleChange">
-			<option v-for="{ name, id } in categories" :key="id" :value="name" :selected="name === selectedCategory">
-				{{ name }}
-			</option>
-		</select>
-		<ul v-if="products.length">
-			<li v-for="product in products" :key="`item-${product.id}`">
-				{{ product.id }}
-				{{ product.title || "Product" }}
-			</li>
-		</ul>
+		<FilterRow />
 	</main>
 </template>

d6d52f07a23b73c6c1dbc7ac9d8f9ade815dd6de
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-16 11:20:56 +0100

    feat: added checkbox and third comboBox

--- src/components/FilterRow.vue
+++ src/components/FilterRow.vue
@@ -7,9 +7,11 @@
 	import type { Option } from "@/types/ComboBox"
 
 	import ComboBox from "./ComboBox.vue"
-
-	const { setSelectedCategory, setFilter, getProducts } = useProducts()
+	import CheckBox from "./CheckBox.vue"
+	const { setSelectedCategory, setFilter, getProducts, getAppliedFilters } = useProducts()
 	const { categories, getSelectedCategory, filters } = storeToRefs(useProducts())
+
+	const emits = defineEmits<{ "update:filters": [value: Record<string, string>[]] }>()
 
 	const categoriesOptions = computed(() => categories.value.map(({ name, id }) => ({ label: name, value: id })))
 
@@ -18,40 +20,74 @@
 		getProducts()
 	}
 	const handleChangeFilter = (filter: Filter, { label, value }: Option) => {
+		if (filter === "price_min" && value) {
+			setFilter("price_max", defaultFiltersPrice[defaultFiltersPrice.length - 1])
+		}
 		setFilter(filter, { label, value })
 		getProducts()
+		emits("update:filters", getAppliedFilters())
 	}
 </script>
 
 <template>
 	<div class="filter-row">
-		<ComboBox
-			v-if="categoriesOptions.length && getSelectedCategory"
-			id="category-filter"
-			label="Filter by"
-			:resetValue="categoriesOptions[0]"
-			:options="categoriesOptions"
-			:modelValue="getSelectedCategory"
-			@update:modelValue="({ label }) => handleChangeCategory(label as CategoryName)"
-		/>
+		<div class="filter-row__combo-boxes">
+			<ComboBox
+				v-if="categoriesOptions.length && getSelectedCategory"
+				id="category-filter"
+				label="Filter by"
+				:resetValue="categoriesOptions[0]"
+				:options="categoriesOptions"
+				:modelValue="getSelectedCategory"
+				@update:modelValue="({ label }) => handleChangeCategory(label as CategoryName)"
+			/>
 
-		<ComboBox
-			v-if="defaultFiltersPrice.length && getSelectedCategory"
-			id="minprice-filter"
-			label="Filter by min price"
-			:resetValue="{ label: 'Prezzo Minimo', value: '' }"
-			:options="defaultFiltersPrice"
-			:modelValue="filters.priceMin"
-			@update:modelValue="(option) => handleChangeFilter('priceMin', option)"
-		/>
+			<ComboBox
+				v-if="defaultFiltersPrice.length && getSelectedCategory"
+				id="minprice-filter"
+				label="Filter by min price"
+				:resetValue="{ label: 'Prezzo Minimo', value: '' }"
+				:options="defaultFiltersPrice"
+				:modelValue="filters.price_min"
+				@update:modelValue="(option) => handleChangeFilter('price_min', option)"
+			/>
+
+			<ComboBox
+				v-if="defaultFiltersPrice.length && getSelectedCategory"
+				id="maxprice-filter"
+				label="Filter by max price"
+				:resetValue="
+					filters.price_min.value
+						? defaultFiltersPrice[defaultFiltersPrice.length - 1]
+						: { label: 'Prezzo Massimo', value: '' }
+				"
+				:options="defaultFiltersPrice"
+				:modelValue="filters.price_max"
+				@update:modelValue="(option) => handleChangeFilter('price_max', option)"
+			/>
+		</div>
+
+		<div class="filter-row__only-available">
+			<!-- there isn't a filter for this in the API
+			but it could be implemented with an extra query param
+			
+			@change="handleChangeFilter('only_available', $event)"
+			-->
+			<CheckBox label="Solo disponibili" id="only-available" />
+		</div>
 	</div>
 </template>
 
 <style scoped lang="scss">
 	.filter-row {
 		display: flex;
+		justify-content: space-between;
 		align-items: center;
-		gap: 1rem;
-		padding: 1rem;
+		&__combo-boxes {
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+			height: 4rem;
+		}
 	}
 </style>

be2544877678c334f61b0319867e5c1a2d110d8a
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-16 11:18:20 +0100

    feat: added availableFiltersMap ,defaultFiltersPrice, currency and fake filter only_available. Added mock values in products discount, colorsLenght and isNew

--- src/stores/products.ts
+++ src/stores/products.ts
@@ -6,26 +6,42 @@ import axios from "@/utils/axios"
 
 const availableCategories = ["Clothes", "Shoes", "Fashion Clothes"]
 
+const availableFiltersMap: Record<string, string> = {
+	price_min: "Prezzo minimo",
+	price_max: "Prezzo massimo",
+}
+const getSevenDaysAgo = () => new Date(new Date().setDate(new Date().getDate() - 7))
+
 export const defaultFiltersPrice = [
+	{ label: "10 €", value: 10 },
+	{ label: "20 €", value: 20 },
 	{ label: "50 €", value: 50 },
 	{ label: "100 €", value: 100 },
 	{ label: "200 €", value: 200 },
 	{ label: "300 €", value: 300 },
 	{ label: "400 €", value: 400 },
 	{ label: "500 €", value: 500 },
+	{ label: "600 €", value: 600 },
+	{ label: "800 €", value: 800 },
+	{ label: "1000 €", value: 1000 },
+	{ label: "10000 €", value: 10000 },
+	{ label: "500000 €", value: 500000 },
 ]
+
+export const currency = "€"
 
 export const useProducts = defineStore("products", {
 	state: () => {
 		return {
 			products: [] as Product[],
 			categories: [] as Category[],
-			selectedCategory: "Shoes" as CategoryName,
+			selectedCategory: "Clothes" as CategoryName,
 			offset: 0,
 			limit: 10,
 			filters: {
-				priceMin: {} as Option,
-				priceMax: {} as Option,
+				price_min: {} as Option,
+				price_max: {} as Option,
+				only_available: {} as Option, //there ins't a filter for this in the API
 			},
 		}
 	},
@@ -62,9 +78,13 @@ export const useProducts = defineStore("products", {
 
 			let { data } = await axios.get(path)
 			//this map is to change the images of the products that are broken in the api
+
 			data = data.map((product: Product) => {
 				return {
 					...product,
+					discount: Math.floor(Math.random() * 48),
+					colorsLength: Math.floor(Math.random() * 5 + 1),
+					isNew: new Date(product.creationAt) > getSevenDaysAgo(),
 					images: product.images.map((image: string) => {
 						if (image.includes("https://placeimg.com")) {
 							return `https://picsum.photos/200/300?&random=${Math.floor(Math.random() * 100)}`
@@ -91,6 +111,17 @@ export const useProducts = defineStore("products", {
 			const { data } = await axios.get(path)
 
 			return data
+		},
+		getAppliedFilters(): Record<string, string>[] | [] {
+			const filters: Record<string, string>[] = Object.entries(this.filters)
+				.filter(([key, value]) => {
+					if (value.value || value.value === 0) {
+						return { [key]: value.label }
+					}
+				})
+				.map(([key, value]) => ({ label: availableFiltersMap[key], value: value.label }))
+
+			return filters || []
 		},
 	},
 })

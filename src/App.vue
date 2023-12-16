6548ed0600b9dfc5d79f43c57b232fccd6c57579
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-16 18:47:02 +0100

    feat: added headerRow

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-	import { RouterLink, RouterView } from "vue-router"
+	import { RouterView } from "vue-router"
+	import HeaderRow from "~/HeaderRow.vue"
 </script>
 
 <template>
-	<header>
-		<div class="wrapper">
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-			</nav>
-		</div>
-	</header>
-
+	<HeaderRow />
 	<RouterView />
 </template>
 

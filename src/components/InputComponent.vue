7a0afa8f4fe9734b467a322d0721e345574b9ac6
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-17 10:37:02 +0100

    feat: added inputComponent

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+	defineEmits<{ "update:modelValue": [value: Event] }>()
+
+	defineProps({
+		label: {
+			type: String,
+			required: true,
+		},
+		id: {
+			type: String,
+			required: true,
+		},
+		labelHidden: {
+			type: Boolean,
+		},
+		placeholder: {
+			type: String,
+			default: "",
+		},
+	})
+
+	//TODO: add search icon
+</script>
+<template>
+	<div class="input">
+		<label :class="{ 'sr-only': labelHidden }" :for="`${id}-input`">{{ label }}</label>
+		<input
+			class="input__input"
+			:id="`${id}-input`"
+			:placeholder="placeholder"
+			type="text"
+			@input="$emit('update:modelValue', $event)"
+		/>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	.input {
+		display: flex;
+		flex-direction: column;
+		&__input {
+			border: 1px solid var(--lightGrey);
+			border-radius: 0.3125rem;
+			padding: 0.5rem;
+			font-size: 1rem;
+			&:focus {
+				outline: none;
+				border-color: var(--primary);
+			}
+		}
+	}
+</style>

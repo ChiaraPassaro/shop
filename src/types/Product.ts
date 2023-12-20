62c7805e05e702da1c2c60733008b99f52addbef
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-20 09:00:27 +0100

    feat: added props product type

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -23,10 +23,14 @@ export type Product = {
 	category: Category
 	creationAt: string
 	images: string[]
+	code: string
 	brand?: string
 	discount?: number
 	colorsLength?: number
 	isNew?: boolean
+	guarantee?: number
+	available?: boolean
+	strengths?: string[]
 }
 
 export type Filter = "price_min" | "price_max" | "only_available"

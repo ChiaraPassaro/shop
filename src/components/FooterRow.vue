1b3359074765eb151421224b442e4bb8cf2e57f7
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-22 15:31:33 +0100

    feat: Add language selection and payment options to footer

--- src/components/FooterRow.vue
+++ src/components/FooterRow.vue
@@ -1,6 +1,45 @@
 <script setup lang="ts">
+	import { storeToRefs } from "pinia"
+	import { useLanguage } from "@/stores/useLanguage"
+
+	import { getImage } from "@/utils/commons"
+
+	import { RouterLink } from "vue-router"
 	import InputComponent from "~/InputComponent.vue"
 	import CheckBox from "~/CheckBox.vue"
+	import ComboBox from "./ComboBox.vue"
+
+	const payments = [
+		{ logo: "./logos/american.png", alt: "American Express" },
+		{ logo: "./logos/cartasi.png", alt: "Carta Si" },
+		{ logo: "./logos/maestro.png", alt: "Maestro" },
+		{ logo: "./logos/paypal.png", alt: "Paypal" },
+		{ logo: "./logos/postepay.png", alt: "Poste Pay" },
+		{ logo: "./logos/visa.png", alt: "Visa" },
+	]
+
+	const links = [
+		[
+			{ name: "Home", path: "" },
+			{ name: "Chi siamo", path: "" },
+			{ name: "Contatti", path: "" },
+			{ name: "Lavora con noi", path: "" },
+		],
+
+		[
+			{ name: "Condizioni di vendita", path: "" },
+			{ name: "Pagamenti", path: "" },
+			{ name: "Certificazioni", path: "" },
+			{ name: "Sitemap", path: "" },
+		],
+		[
+			{ name: "Privacy policy", path: "" },
+			{ name: "Cookie policy", path: "" },
+			{ name: "Credits", path: "" },
+		],
+	]
+
+	const { selectedLang, languages } = storeToRefs(useLanguage())
 </script>
 
 <template>
@@ -25,8 +64,40 @@
 				</div>
 			</form>
 		</div>
-		<div class="footer__middle"></div>
-		<div class="footer__bottom"></div>
+		<div class="footer__middle">
+			<div class="footer__middle-logo">
+				<RouterLink class="header__main-logo" to="/">
+					<img src="@/assets/logo.svg" alt="logo" />
+				</RouterLink>
+			</div>
+
+			<div class="footer__middle-links">
+				<ul v-for="(list, listIdx) in links" :key="`list-${listIdx}`" class="footer__middle-links-list list-none">
+					<li v-for="({ path, name }, idx) in list" :key="`link-${idx}`">
+						<RouterLink :to="path">{{ name }}</RouterLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer__bottom">
+			<ul class="footer__bottom-payments list-none">
+				<li v-for="({ logo, alt }, i) in payments" :key="`payment-${i}`" class="footer__bottom-payments-item">
+					<img :src="getImage(logo).src" :alt="alt" />
+				</li>
+			</ul>
+			<ComboBox
+				id="language"
+				class="header__top-language"
+				:modelValue="selectedLang"
+				:options="languages"
+				label="language"
+				borderless
+			/>
+		</div>
+		<div class="footer__copy">
+			© 2023 Logo – Via delle Albere 13, 36045 Vicenza P.IVA 00589040245 – Registro Imprese di Vicenza: n. 00589040245
+			– Rea vi: 139951 Capitale sociale: € 10.000.000 i.v.
+		</div>
 	</div>
 </template>
 
@@ -46,11 +117,15 @@
 				display: flex;
 				gap: 1rem;
 				align-items: center;
+				& > * {
+					width: calc(100% / 3);
+				}
 			}
 			&-privacy {
 				display: flex;
-				align-items: center;
+				align-items: flex-start;
 				gap: 0.62rem;
+				font-size: 0.85rem;
 			}
 
 			@media (max-width: 896px) {
@@ -69,6 +144,74 @@
 					}
 				}
 			}
+		}
+		&__middle {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 2.69rem 6.5rem;
+			background-color: var(--veryLightGrey);
+			gap: 1.5rem;
+
+			@media (max-width: 896px) {
+				flex-direction: column;
+				align-items: flex-start;
+				padding: 2.69rem 1.5rem;
+			}
+
+			&-links {
+				display: grid;
+				grid-template-columns: repeat(3, 1fr);
+				gap: 3.75rem;
+				@media (max-width: 896px) {
+					grid-template-columns: 1fr;
+					gap: 1.75rem;
+				}
+
+				li {
+					text-align: right;
+					a {
+						color: var(--black);
+						text-decoration: none;
+						&:hover {
+							color: var(--primary);
+						}
+					}
+					@media (max-width: 896px) {
+						width: 100%;
+						text-align: left;
+					}
+				}
+			}
+		}
+		&__bottom {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 2.69rem 6.5rem;
+			@media (max-width: 896px) {
+				flex-direction: column;
+				&-item {
+					height: 1.25rem;
+				}
+			}
+			&-payments {
+				display: flex;
+				align-items: center;
+				gap: 1.5rem;
+				&-item {
+					height: 1.75rem;
+					img {
+						width: 100%;
+					}
+				}
+			}
+		}
+		&__copy {
+			padding: 1.5rem 6.5rem;
+			font-size: 0.75rem;
+			text-align: center;
+			border-top: 1px solid var(--veryLightGrey);
 		}
 	}
 </style>

7259106e44abc3bc62161a8c153f26cc8cb63c76
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-12-22 12:22:26 +0100

    Feta: Add FooterRow component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 	import { RouterView } from "vue-router"
 	import HeaderRow from "~/HeaderRow.vue"
+	import FooterRow from "~/FooterRow.vue"
 </script>
 
 <template>
 	<HeaderRow />
 	<RouterView />
+	<FooterRow />
 </template>
 
 <style scoped></style>
